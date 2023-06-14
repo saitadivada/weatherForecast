b5579f14f901a5148aa0579efd9f01e0fa6dcf81
saitadivada <saitadivada07@gmail.com>
2023-06-14 11:56:39 +0530

    Handled error case for  entered city which is not present

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ export class AppComponent {
   weatherSearchForm: FormGroup;
   weatherforecastData: any;
   loader: boolean = false;
-  nodataFound :boolean = true;
+  nodataFound: boolean = true;
+  errorMessage: any;
+  cityNotFound: boolean = false;
   constructor(private apiservice: ApiService) {
     this.weatherSearchForm = new FormGroup({
       location: new FormControl('', [Validators.required])
@@ -26,14 +28,22 @@ export class AppComponent {
       let weatherformobj = {
         location: this.weatherSearchForm.value.location
       }
-      this.apiservice.getforeCastData(weatherformobj.location).subscribe((resp: any) => {
-        if(resp.cod == 200) {
-          this.weatherforecastData = resp.list.slice(0, 15);
+      this.apiservice.getforeCastData(weatherformobj.location).subscribe(
+        (resp: any) => {
+          if (resp.cod == 200) {
+            this.weatherforecastData = resp.list.slice(0, 20);
+            this.loader = false;
+            this.cityNotFound = false;
+          }
+          else {}
+        },
+        (error) => {
           this.loader = false;
-        }
-      })
-    }
-    else {
+          this.errorMessage = error.error.message;
+          this.cityNotFound = true;
+          this.weatherforecastData = [];
+        },
+      )
     }
   }
 
